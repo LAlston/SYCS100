1c34020e2cfee7753b94660513dcc12496925b7d
LAlston <lena.alston@bison.howard.edu>
2013-11-17 17:44:40 -0500

    Lena added her bsearch!

--- projects/bsearch/RealSeptimalsbseach.py
+++ projects/bsearch/RealSeptimalsbseach.py
@@ -1,4 +1,3 @@
-
 # Alston Clark
 
 def bsearch(list,search):
@@ -33,8 +32,29 @@ def bsearch(list,search):
     else:
       return mid
 
+#Lena Alston 
 
-    
+def bsearch(list, element):              #bsearch function definition 
+    if len(list) != 0:                     #condition checking for emptiness 
+        first = 0                   
+        last = len(list)-1                #declaration of last index variable 
+        found = False                        #bool found variable defined 
+        while not found:                  #while loops until element is found 
+            midpoint = (first + last)/2         #updates midpoint checking
+            if (first==last) and (element != list[first]):      #when one element is left to be searched and that element is not the desired value   
+                print 'Element is not in list.'     
+                return -1                                   #ends function 
+            elif element < list[midpoint]:              #if element is less than element at current list position 
+                last = midpoint - 1                     #updates last index value 
+            elif element > list[midpoint]:              #if element is greater than element at current list positon 
+                first = midpoint + 1                    #updates first index value 
+            else:                                       #else element equals element at current list positon 
+                found = True                        #ends while loop 
+                return midpoint     #returns index of searched element 
+    else:                           #list is empty 
+        print 'Element cannot be found. List is empty.'
+        return -1
+
 
 
 
