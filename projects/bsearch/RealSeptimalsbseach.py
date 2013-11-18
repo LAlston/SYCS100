202b59eb13bc8b07a2a8da1fd74c1674db035d09
LAlston <lena.alston@bison.howard.edu>
2013-11-17 22:00:28 -0500

    Update RealSeptimalsbseach.py

--- projects/bsearch/RealSeptimalsbseach.py
+++ projects/bsearch/RealSeptimalsbseach.py
@@ -34,24 +34,24 @@ def bsearch(list,search):
 
 #Lena Alston 
 
-def bsearch(list, element):              #bsearch function definition 
-    if len(list) != 0:                     #condition checking for emptiness 
+def bsearch(list, element):              
+    if len(list) != 0:                     
         first = 0                   
-        last = len(list)-1                #declaration of last index variable 
-        found = False                        #bool found variable defined 
-        while not found:                  #while loops until element is found 
-            midpoint = (first + last)/2         #updates midpoint checking
-            if (first==last) and (element != list[first]):      #when one element is left to be searched and that element is not the desired value   
+        last = len(list)-1          
+        found = False                   
+        while not found:                
+            midpoint = (first + last)/2         
+            if (first==last) and (element != list[first]):  
                 print 'Element is not in list.'     
-                return -1                                   #ends function 
-            elif element < list[midpoint]:              #if element is less than element at current list position 
-                last = midpoint - 1                     #updates last index value 
-            elif element > list[midpoint]:              #if element is greater than element at current list positon 
-                first = midpoint + 1                    #updates first index value 
-            else:                                       #else element equals element at current list positon 
-                found = True                        #ends while loop 
-                return midpoint     #returns index of searched element 
-    else:                           #list is empty 
+                return -1                                   
+            elif element < list[midpoint]:       
+                last = midpoint - 1                   
+            elif element > list[midpoint]:            
+                first = midpoint + 1                
+            else:                                   
+                found = True                      
+                return midpoint     
+    else:                           
         print 'Element cannot be found. List is empty.'
         return -1
 
